ab4cf618163d7e4d53bad683bb299a41e11573dd
aspectrr <collin@steelbrowser.com>
2026-08-28 15:45:52 -0400

    ASPTR-189: SVG playing cards, CardRow 3D flip, deal/flip/win-pulse/chip-fly animations, /cards demo route

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Router } from '@solidjs/router'
 import { LobbyPage } from './pages/Lobby'
 import { AuthPage } from './pages/Auth'
+import { CardsPage } from './pages/Cards'
 
 function App() {
   return (
     <Router>
       <Route path="/" component={LobbyPage} />
       <Route path="/auth" component={AuthPage} />
+      <Route path="/cards" component={CardsPage} />
     </Router>
   )
 }
